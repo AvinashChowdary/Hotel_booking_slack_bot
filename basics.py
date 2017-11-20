38a9c56c02449251d94513a0bf84f860aefda5ce
Avinash Ravilla <avinash@Avinashs-MacBook-Air.local>
2017-11-20 13:04:35 -0800

    added chatterbot to perform basic conversations

---
+++ basics.py
@@ -0,0 +1,35 @@
+from chatterbot import ChatBot
+
+class basics(object):
+
+    def __init__(self):
+        self.bot = ChatBot(
+            'Hotel Booking Bot',
+            logic_adapters=[
+                "chatterbot.logic.MathematicalEvaluation",
+                "chatterbot.logic.TimeLogicAdapter",
+                "chatterbot.logic.BestMatch"
+            ],
+            input_adapter="chatterbot.input.VariableInputTypeAdapter",
+            output_adapter="chatterbot.output.OutputAdapter",
+            trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+      )
+
+        # self.bot.train('chatterbot.corpus.english')
+        self.bot.train('chatterbot.corpus.english.greetings')
+        self.bot.train('chatterbot.corpus.english.conversations')
+        # self.bot.train('chatterbot.corpus.english.ai')
+        # self.bot.train('chatterbot.corpus.english.computers')
+        # self.bot.train('chatterbot.corpus.english.emotion')
+        # self.bot.train('chatterbot.corpus.english.history')
+        # self.bot.train('chatterbot.corpus.english.humor')
+        # self.bot.train('chatterbot.corpus.english.literature')
+        # self.bot.train('chatterbot.corpus.english.money')
+        # self.bot.train('chatterbot.corpus.english.psychology')
+        # self.bot.train('chatterbot.corpus.english.science')
+        # self.bot.train('chatterbot.corpus.english.sports')
+        # self.bot.train('chatterbot.corpus.english.trivia')
+
+    def respond(self,sentence):
+        response = self.bot.get_response(sentence)
+        return response
