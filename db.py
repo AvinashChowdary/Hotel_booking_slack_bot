277fd95b4ffcaa9539d4d9855481f42f5779244c
Avinash Ravilla <avinash@Avinashs-MacBook-Air.local>
2017-12-06 18:17:03 -0800

    added code to save transaction to mongo db

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 class bot_db(object):
     def __init__(self):
@@ -16,3 +17,16 @@ class bot_db(object):
         for obj in self.db.hotels.find():
             response.append(obj)
         return response
+
+    def get_booking(self, key):
+        hotel = self.db.hotels
+        booking = hotel.find({"booking_id": key })
+        reply = dumps(booking)
+        print(reply)
+        return reply
+
+    def delete_booking(self, id):
+        hotel = self.db.hotels
+        removed = hotel.remove({"booking_id" : id})
+        print(removed)
+        return 1
